52d3ac473aa180217c993107df828941ad51a3ed
Vamsi Mahesh <mahesh@digifutura.com>
2016-08-25 10:07:21 +0530

    Basic datatypes in c

---
+++ data_types.c
@@ -0,0 +1,16 @@
+// simple program to declare all the primitive types and display them
+#include<stdio.h>
+
+int main(){
+
+	int a = 1;
+	float real_number = 3.14;
+	char c = 'c';
+
+
+	printf("a = %d",a);
+	printf("real_number is %f",real_number);
+	printf("c= %c",c);
+
+	return 0;
+}
